d4584748c4a6e15cbe1bb5b19dc16dae891d59ae
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-12-12 11:32:17 -0500

    updates

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,7 +1,8 @@
-import { encodeBase32LowerCaseNoPadding } from "@oslojs/encoding";
+import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
 import { sha256 } from "@oslojs/crypto/sha2"
 
 import type { User, Session } from "@prisma/client";
+import prisma from "$lib/db";
 
 export function generateSessionToken(): string {
   const bytes = new Uint8Array(20);
@@ -16,5 +17,41 @@ export async function createSession(token: string, userId: string): Promise<Sess
     id: sessionId,
     userId,
     expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
-  }
+  };
+  await prisma.session.create({ data: session });
+  return session;
 }
+
+export async function validateSessionToken(token: string): Promise<SessionValidationResult> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const result = await prisma.session.findUnique({
+    where: {
+      id: sessionId
+    },
+    include: { 
+      user: true
+    }
+  });
+  if (result === null) {
+    return { session: null, user: null };
+  }
+  const { user, ...session } = result;
+  if (Date.now() >= session.expiresAt.getTime()) {
+    await prisma.session.delete({ where: { id: sessionId } });
+    return { session: null, user: null };
+  }
+  if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 60 * 24 * 15) {
+    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+    await prisma.session.update({
+      where: {
+        id: session.id
+      },
+      data: {
+        expiresAt: session.expiresAt
+      }
+    });
+  }
+  return { session, user };
+}
+
+export type SessionValidationResult = | { session: Session; user: User } | { session: null; user: null };
---
+++ src/lib/db.ts
@@ -0,0 +1,8 @@
+import { env } from "$env/dynamic/private";
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
