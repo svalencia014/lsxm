ddd80a22fea5cdf21222f0c0fe925c92f96ffb06
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-12-13 09:13:33 -0500

    finish authentication

--- src/lib/lastfm.ts
+++ src/lib/lastfm.ts
@@ -1,3 +1,10 @@
-export async function getApiSignature(parameters: string): Promise<string> {
-  const rawstring = "";
+
+import { env } from "$env/dynamic/private"
+import crypto from "crypto"
+
+export async function getApiSignature(parameters: string, api_key: string): Promise<string> {
+  const rawString = `api_key${api_key}${parameters}${env.SECRET}`;
+  const signature = crypto.createHash("md5").update(rawString).digest("hex");
+  console.log(signature)
+  return signature;
 }
--- src/routes/login/callback/+server.ts
+++ src/routes/login/callback/+server.ts
@@ -1,4 +1,8 @@
+import { env } from "$env/dynamic/public";
 import type { RequestEvent } from "./$types";
+import { getApiSignature } from "$lib/lastfm";
+import prisma from "$lib/db";
+import { createSession, generateSessionToken, setSessionTokenCookie } from "$lib/auth/auth";
 
 export async function GET(event: RequestEvent): Promise<Response> {
   const token = event.url.searchParams.get("token");
@@ -9,4 +13,46 @@ export async function GET(event: RequestEvent): Promise<Response> {
   }
 
   console.log(token);
+  const api_sig = await getApiSignature(`methodauth.getSessiontoken${token}`, env.PUBLIC_API_KEY);
+  console.log(env.PUBLIC_API_KEY);
+  const req = await fetch(`https://ws.audioscrobbler.com/2.0/?method=auth.getSession&token=${token}&api_key=${env.PUBLIC_API_KEY}&api_sig=${api_sig}&format=json`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  })
+
+  const res: lastFmSessionResponse = await req.json();
+
+  await prisma.user.upsert({
+    create: {
+      id: res.session.name,
+      key: res.session.key
+    },
+    update: {
+      key: res.session.key
+    },
+    where: {
+      id: res.session.name
+    }
+  })
+    
+  const sessionToken = generateSessionToken();
+  const session = await createSession(sessionToken, res.session.name);
+  setSessionTokenCookie(event, sessionToken, session.expiresAt);
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "/"
+    }
+  });
+}
+
+type lastFmSessionResponse = {
+  session: {
+    name: string;
+    key: string;
+    subscriber: number;
+  }
 }
